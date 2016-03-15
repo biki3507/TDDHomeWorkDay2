885cab0d16740a9e33f7a0c9ce2075544574daa5
david <biki3507@gmail.com>
2016-03-16 00:07:45 +0800

    Scenario1

---
+++ Day2_Homework/PotterShoppingCart.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2_Homework
+{
+    public class PotterShoppingCart
+    {
+        public double GetPrice()
+        {
+
+            return 100;
+        }
+
+    }
+}
