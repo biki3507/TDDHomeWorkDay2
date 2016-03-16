1e87f843542e87eb1ae3c07bddc3bd6e256c0be9
David <david@miniasp.com>
2016-03-16 17:04:48 +0800

    改變discount算法

--- Day2_Homework/PotterShoppingCart.cs
+++ Day2_Homework/PotterShoppingCart.cs
@@ -14,42 +14,39 @@ namespace Day2_Homework
             double totalPrice = 0;
             this.books = Books;
             totalPrice += GetDiscount(totalPrice);
-
-
             totalPrice += Books.Count * 100;
             return totalPrice;
         }
 
         private double GetDiscount(double totalPrice)
         {
-            if (this.books.Count(x => x.Version == 1) > 0 && this.books.Count(y => y.Version == 2) > 0 && this.books.Count(y => y.Version == 3) > 0 && this.books.Count(y => y.Version == 4) > 0 && this.books.Count(y => y.Version == 5) > 0)
+
+            if (this.books.GroupBy(x => x.Version).Select(y => y.FirstOrDefault()).Count() ==5)
             {
-                this.books.Remove(this.books.FirstOrDefault(x => x.Version == 1));
-                this.books.Remove(this.books.FirstOrDefault(x => x.Version == 2));
-                this.books.Remove(this.books.FirstOrDefault(x => x.Version == 3));
-                this.books.Remove(this.books.FirstOrDefault(x => x.Version == 4));
-                this.books.Remove(this.books.FirstOrDefault(x => x.Version == 5));
+                foreach (var item in this.books.GroupBy(x => x.Version).Select(y => y.FirstOrDefault()))
+                {
+                    this.books.Remove(item);
+                }
                 totalPrice += 5 * (100 * 0.75);
             }
-            if (this.books.Count(x => x.Version == 1) > 0 && this.books.Count(y => y.Version == 2) > 0 && this.books.Count(y => y.Version == 3) > 0 && this.books.Count(y => y.Version == 4) > 0)
+            if (this.books.GroupBy(x => x.Version).Select(y => y.FirstOrDefault()).Count() ==4)
             {
-                this.books.Remove(this.books.FirstOrDefault(x => x.Version == 1));
-                this.books.Remove(this.books.FirstOrDefault(x => x.Version == 2));
-                this.books.Remove(this.books.FirstOrDefault(x => x.Version == 3));
-                this.books.Remove(this.books.FirstOrDefault(x => x.Version == 4));
+                foreach (var item in this.books.GroupBy(x => x.Version).Select(y => y.FirstOrDefault()))
+                {
+                    this.books.Remove(item);
+                }
                 totalPrice += 4 * (100 * 0.8);
             }
-            if (this.books.Count(x => x.Version == 1) > 0 && this.books.Count(y => y.Version == 2) > 0 && this.books.Count(y => y.Version == 3) > 0)
+            if (this.books.GroupBy(x => x.Version).Select(y => y.FirstOrDefault()).Count() == 3)
             {
-                this.books.Remove(this.books.FirstOrDefault(x => x.Version == 1));
-                this.books.Remove(this.books.FirstOrDefault(x => x.Version == 2));
-                this.books.Remove(this.books.FirstOrDefault(x => x.Version == 3));
+                foreach (var item in this.books.GroupBy(x => x.Version).Select(y => y.FirstOrDefault()))
+                {
+                    this.books.Remove(item);
+                }
                 totalPrice += 3 * (100 * 0.90);
             }
             if (this.books.GroupBy(x => x.Version).Select(y => y.FirstOrDefault()).Count() == 2)
             {
-                //this.books.Remove(this.books.FirstOrDefault(x => x.Version == 1));
-                //this.books.Remove(this.books.FirstOrDefault(x => x.Version == 2));
                 foreach (var item in this.books.GroupBy(x => x.Version).Select(y => y.FirstOrDefault()))
                 {
                     this.books.Remove(item);
