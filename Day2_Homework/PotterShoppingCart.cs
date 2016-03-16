47b08b9cdc3e41cdd89021186186d84ede441944
David <david@miniasp.com>
2016-03-16 16:10:40 +0800

    找到可行方式

--- Day2_Homework/PotterShoppingCart.cs
+++ Day2_Homework/PotterShoppingCart.cs
@@ -20,7 +20,7 @@ namespace Day2_Homework
             return totalPrice;
         }
 
-        private  double GetDiscount(double totalPrice)
+        private double GetDiscount(double totalPrice)
         {
             if (this.books.Count(x => x.Version == 1) > 0 && this.books.Count(y => y.Version == 2) > 0 && this.books.Count(y => y.Version == 3) > 0 && this.books.Count(y => y.Version == 4) > 0 && this.books.Count(y => y.Version == 5) > 0)
             {
@@ -46,10 +46,14 @@ namespace Day2_Homework
                 this.books.Remove(this.books.FirstOrDefault(x => x.Version == 3));
                 totalPrice += 3 * (100 * 0.90);
             }
-            if (this.books.Count(x => x.Version == 1) > 0 && this.books.Count(y => y.Version == 2) > 0)
+            if (this.books.GroupBy(x => x.Version).Select(y => y.FirstOrDefault()).Count() == 2)
             {
-                this.books.Remove(this.books.FirstOrDefault(x => x.Version == 1));
-                this.books.Remove(this.books.FirstOrDefault(x => x.Version == 2));
+                //this.books.Remove(this.books.FirstOrDefault(x => x.Version == 1));
+                //this.books.Remove(this.books.FirstOrDefault(x => x.Version == 2));
+                foreach (var item in this.books.GroupBy(x => x.Version).Select(y => y.FirstOrDefault()))
+                {
+                    this.books.Remove(item);
+                }
                 totalPrice += 2 * (100 * 0.95);
             }
             return totalPrice;
@@ -62,5 +66,5 @@ namespace Day2_Homework
         public int Num { get; set; }
         public int UnitPrice { get; set; }
     }
-   
+
 }
